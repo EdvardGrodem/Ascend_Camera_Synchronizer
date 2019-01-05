a5384b012e63bda57e52847455e745746e9ec2ba
EdvardGrodem <edvard.grodem@gmail.com>
2019-01-05 10:31:04 +0100

    Inital commit of Atmel Studio files

---
+++ Camera_Synchronizator/Camera_Synchronizator/main.c
@@ -0,0 +1,18 @@
+/*
+ * Camera_Synchronizator.c
+ *
+ * Created: 05.01.2019 10:27:36
+ * Author : EdvardOlaf
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
